4c5b0b92784b2c23b82fae5663352da43ba46d23
GenzoWeb <genzo10@hotmail.fr>
2023-01-31 18:30:47 +0100

    Homepage / reset CSS

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ForumController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/forum', name: 'forum')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-
+        return $this->render('forum/index.html.twig', [
+            'controller_name' => 'ForumController',
         ]);
     }
 }
